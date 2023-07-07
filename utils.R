9337a815a1d5dd0c32f270ae421c5d2ff5a1b5ac
Hubert Thieriot <hubert.thieriot@gmail.com>
2023-07-07 17:37:25 +0000

    -adding function to clean met files

--- utils.R
+++ utils.R
@@ -7,3 +7,43 @@ remove_small_cache_files <- function(cache_folder='cache', min_bytes=200){
         }
     }
 }
+
+remove_incomplete_cache_and_met_files <- function(){
+
+  # Count rows in files
+  file_rows <- pbapply::pblapply(
+    list.files('cache', pattern='dispersion*', full.names = T),
+    function(file){
+      tibble(file=file,
+             n=nrow(readRDS(file)))
+    })
+
+  file_rows <- file_rows %>% bind_rows()
+
+  incomplete_files <- file_rows %>%
+    filter(n < 0.9 * max(n))
+
+  # For each small file extract the date
+  dates <- incomplete_files %>%
+    mutate(date = str_extract(file, '[0-9]{8}')) %>%
+    select(date) %>%
+    distinct() %>%
+    pull()
+
+  date_to_filename <- function(date){
+    # generate gdas1.jul13.w2
+    date <- as.Date(date, format='%Y%m%d')
+    monthyear <- tolower(format(date, '%b%y'))
+    day <- as.numeric(format(date, '%d'))
+    # integer division
+    week <- floor(day / 7) + 1
+    file.path(
+      Sys.getenv('DIR_HYSPLIT_MET'),
+      paste0('gdas1.', monthyear, '.w', week))
+  }
+
+  filenames <- dates %>% date_to_filename() %>% unique()
+  file.remove(filenames)
+  file.remove(incomplete_files$file)
+
+}
